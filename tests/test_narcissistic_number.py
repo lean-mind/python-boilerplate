1a247674e4a40ba0458014c6507def277a4983d8
Wolfremium13 <kevin@leanmind.es>
2023-02-06 19:38:19 +0000

    🚨 isort lint

--- tests/test_narcissistic_number.py
+++ tests/test_narcissistic_number.py
@@ -1,11 +1,14 @@
-from assertpy import assert_that
 from unittest import TestCase
+
+from assertpy import assert_that
 
 from src.narcissistic_number import is_narcissistic
 
 
 class TestIsNarcissisticNumber(TestCase):
-    def test_is_narcissistic_if_the_sum_of_its_digits_to_the_nth_power_equal_the_original_number(self):
+    def test_is_narcissistic_if_the_sum_of_its_digits_to_the_nth_power_equal_the_original_number(
+        self,
+    ):
         assert_that(is_narcissistic(150)).is_false()
         assert_that(is_narcissistic(0)).is_true()
         assert_that(is_narcissistic(153)).is_true()
